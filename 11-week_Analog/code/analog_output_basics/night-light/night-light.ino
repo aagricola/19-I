427945f469f1ff034eb568aaca83c575629742d0
aagricola@mica.edu <aagricola@mica.edu>
2019-04-03 17:15:22 -0400

    night light

--- 11-week_Analog/code/analog_output_basics/night-light/night-light.ino
+++ 11-week_Analog/code/analog_output_basics/night-light/night-light.ino
@@ -1,9 +1,7 @@
-/*
+//Night Light
+//Interaction, 2019
 
- Night Light
- Lights an LED when the room is dark.
-
- By John Keefe, January 2016
+/*Based on code By John Keefe, January 2016
  http://keefe.cc/night-light
 
  Based on example code created by David Cuartielles
@@ -11,46 +9,35 @@
  http://www.arduino.cc/en/Tutorial/AnalogInput
 
  This code is in the public domain.
-
  */
 
-int sensorPin = A0;   // select the input pin for the photoresistor
-int ledPin = 13;      // select the pin for the LED
-int sensorValue = 0;  // variable to store the value from the sensor
-int darkPoint = 0;    // if the sensor value is less than this, the room is "dark"
+int analog0; // A variable to hold analog input. Analog input will always be a range of numbers between 0~1023
+int ledPin = 13; //select pin for LED
+//first read the Serial.println to find your dark point
+int darkPoint = 600;// if the sensor value is less than this, the room is "dark"
 
-// This code runs once when Arduino is turned on or reset
-void setup() {
-
-  // declare the ledPin as an OUTPUT:
+void setup(){
+  // no pinmode needed for analog
   pinMode(ledPin, OUTPUT);
-
-  // this is needed to use the Serial Monitor
-  Serial.begin(9600);
-
+  Serial.begin(9600); // Start communication for Serial Monitor
 }
 
-// This code loops forever
-void loop() {
-
-  // read the value from the sensor:
-  sensorValue = analogRead(sensorPin);
+void loop(){
+  analog0 = analogRead(0); // Read input from the specified analog pin.
 
   // if the sensorValue is less than the darkPoint
   // power the LED:
-  if (sensorValue < darkPoint) {
+  if (analog0 < darkPoint) {
     digitalWrite(ledPin, HIGH);
   }
 
   // if the sensorValue is greater than or equal to the
   // darkPoint, don't power the LED:
-  if (sensorValue >= darkPoint) {
+  if (analog0 >= darkPoint) {
     digitalWrite(ledPin, LOW);
   }
 
-  // output the sensor value to the Serial Monitor
-  Serial.println(sensorValue);
+  Serial.println(analog0); // Send number to the Serial Monitor
+  delay(100); // Always use a small delay after printing messages
 
-  // add a little delay to help read the numbers!
-  delay(100); 
 }
